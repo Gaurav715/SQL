4fb89269b41a0deb418220c9f16ab25595c80810
Gaurav715 <58346263+Gaurav715@users.noreply.github.com>
2020-03-04 20:27:05 +0100

    Add files via upload

---
+++ old and new.sql
@@ -0,0 +1,17 @@
+#old Syntax of join 
+   SELECT 
+    e.emp_no, e.first_name, e.last_name, dm.dept_no, e.hire_date
+FROM
+    employees e,
+    dept_manager dm
+WHERE
+    e.emp_no = dm.emp_no;
+
+
+# new Syntax of join 
+SELECT
+e.emp_no ,e.first_name, e.last_name, dm.dept_no, e.hire_date
+FROM
+    employees e
+        JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no;
