a79439b8c4b5f5b7c024802869568db112389866
Gaurav715 <58346263+Gaurav715@users.noreply.github.com>
2020-03-05 22:09:10 +0100

    Add files via upload

---
+++ sql and Tableau.sql
@@ -0,0 +1,13 @@
+select distinct
+emp_no, from_date,to_date 
+from t_dept_emp;
+
+select 
+year(d.from_date) as calendar_Year,
+e.gender,
+count(e.emp_no) as num_of_employees
+from t_employees e
+join 
+t_dept_emp d on d.emp_no = e.emp_no
+group by calendar_year,e.gender
+having calendar_year >= 1990;
