f79db3edaa10711e2be983138f90232ac43bb9ec
Gaurav715 <58346263+Gaurav715@users.noreply.github.com>
2020-03-05 20:00:43 +0100

    Add files via upload

--- Index.sql
+++ Index.sql
@@ -1,7 +1,12 @@
 SELECT 
     *
 FROM
-    salaries
+    employees
 WHERE
-    salary > 89000;
-create index i_salary on salaries(salary);
+    hire_date > '2000-01-01';
+    
+    ALTER TABLE employees
+
+DROP INDEX i_hire_date;
+    
+create index i_hire_date on employees(hire_date);
